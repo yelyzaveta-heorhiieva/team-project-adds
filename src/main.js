bcf5673137b97b135e0a8929d799816cb2ed497e
Yelyzaveta Heorhiieva <heorhiieva.y@gmail.com>
2024-09-12 10:36:22 +0300

    add features

--- src/main.js
+++ src/main.js
@@ -16,4 +16,33 @@ open.addEventListener('click',
     close.addEventListener('click',
     function () {
         menu.classList.remove('is-open');
+        });
+    
+                const links =
+    document.querySelectorAll('.nav-link-header');
+
+links.forEach(function (link) {
+    link.addEventListener('click',
+        function (event) {
+            links.forEach(function (link) {
+                link.classList.remove('current');
+            });
+            this.classList.add('current');
+        });
+});
+    
+const drops =
+    document.querySelectorAll('.nav-link-mobile');
+
+drops.forEach(function (drop) {
+    drop.addEventListener('click',
+        function () {
+            menu.classList.remove('is-open');
+        },
+        function (event) {
+           drops.forEach(function (drop) {
+                drop.classList.remove('current');
+            });
+            this.classList.add('current');
+        });
     });
